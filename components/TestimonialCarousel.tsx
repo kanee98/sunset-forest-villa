0dd6025c65e3275493c93a904b04189a1756e8e4
Kanishka Udapitiya <43329812+kanee98@users.noreply.github.com>
2025-07-31 14:21:37 +0530

    Update TestimonialCarousel.tsx

--- components/TestimonialCarousel.tsx
+++ components/TestimonialCarousel.tsx
@@ -106,21 +106,6 @@ export default function TestimonialCarousel() {
           </div>
         ))}
       </div>
-
-      <div className="flex justify-center gap-4 mt-8">
-        <button
-          onClick={() => instanceRef.current?.prev()}
-          className="bg-[#B8860B] text-white px-4 py-2 rounded hover:bg-[#a7770b]"
-        >
-          Prev
-        </button>
-        <button
-          onClick={() => instanceRef.current?.next()}
-          className="bg-[#B8860B] text-white px-4 py-2 rounded hover:bg-[#a7770b]"
-        >
-          Next
-        </button>
-      </div>
     </section>
   );
 }
